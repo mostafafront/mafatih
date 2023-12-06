6c9c9e626739e1acb547194b529ac430908586ae
mostafa <mostafa.h.web@gmail.com>
2023-12-07 00:37:18 +0300

    add header and footer into donate page as layout

--- src/pages/donate/index.js
+++ src/pages/donate/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import HeaderC from '../../../components/header'
+import FooterC from '../../../components/footer'
 
 export default function DonatePage() {
   return (
     <>
+    <HeaderC/>
+    
+    <FooterC/>
     </>
   )
 }
