05ed8a077ca2e16fd1bfe12b5849ddd7c377a0be
mostafa <mostafa.h.web@gmail.com>
2023-12-14 01:10:42 +0300

    create body of accordion in search resault page as second section

--- src/pages/searchResault/index.js
+++ src/pages/searchResault/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import HeaderC from "../../../components/header";
 import FilterInput from "../../../components/index/filterInput";
+import Link from "next/link";
 
 export default function SearchResaultPage() {
   function preventSubmit(event) {
@@ -124,11 +125,11 @@ export default function SearchResaultPage() {
 
         {/* Second Section */}
 
-        <div class="accordion" id="accordionPanelsStayOpenExample">
-          <div class="accordion-item accordion-item-sr">
-            <h2 class="accordion-header accordion-header-sr">
+        <div className="accordion" id="accordionPanelsStayOpenExample">
+          <div className="accordion-item accordion-item-sr">
+            <h2 className="accordion-header accordion-header-sr">
               <button
-                class="accordion-button accordion-button-sr d-flex justify-content-between align-items-center"
+                className="accordion-button accordion-button-sr d-flex justify-content-between align-items-center"
                 type="button"
                 data-bs-toggle="collapse"
                 data-bs-target="#panelsStayOpen-collapseOne"
@@ -141,25 +142,105 @@ export default function SearchResaultPage() {
             </h2>
             <div
               id="panelsStayOpen-collapseOne"
-              class="accordion-collapse collapse show"
+              className="accordion-collapse collapse show"
             >
-              <div class="accordion-body accordion-body-s">
-                <strong>This is the first item's accordion body.</strong> It is
-                shown by default, until the collapse plugin adds the appropriate
-                classes that we use to style each element. These classes control
-                the overall appearance, as well as the showing and hiding via
-                CSS transitions. You can modify any of this with custom CSS or
-                overriding our default variables. It's also worth noting that
-                just about any HTML can go within the{" "}
-                <code>.accordion-body</code>, though the transition does limit
-                overflow.
+              <div className="accordion-body accordion-body-sr d-flex flex-column">
+                <div className="textFind-title p-3">
+                  <div className="titleOf-textFind">
+                    زیارت
+                    <span className="white-space"> </span>
+                    <span className="textFinded">
+                      <Link href={"./"}>عاشورا</Link>
+                    </span>
+                    <span className="white-space"> </span>
+                    امام حسین
+                  </div>
+                </div>
+                <div className="textFind-title-desciption p-3">
+                  <div className="title-textFind">
+                    زیارت امام حسین(ع) در روز
+                    <span className="white-space"> </span>
+                    <span className="textFinded">
+                      <Link href={"./"}>عاشورا</Link>
+                    </span>
+                    <span className="white-space"> </span>
+                  </div>
+                  <div className="description-textFind">
+                    <p>
+                      <span className="three-point">...</span>
+                      <span className="white-space"> </span>
+                      بدان که زیارت روایتشده برای روز{" "}
+                      <span className="textFinded">
+                        <Link href={"./"}>عاشورا</Link>
+                      </span>{" "}
+                      چند زیارت است و ما در اینجا به خاطر رعایت مختصرنویسی فقط
+                      به بیان<span className="three-point">...</span>ما در اینجا
+                      به خاطر رعایت مختصرنویسی فقط به بیان اعمال روز عاشورا نیز
+                      زیارتی را نقل کردم
+                    </p>
+                  </div>
+                </div>
+                <div className="textFind-description p-3">
+                  <div className="title-textFind">
+                    زیارت‌های امام حسین(ع) در ایام خاص
+                  </div>
+                  <div className="description-textFind">
+                    <p>
+                      <span className="three-point">...</span>
+                      <span className="white-space"> </span>
+                      شب های قدر پنجم زیارت آن حضرت در عید فطر و قربان ششم زیارت
+                      آن حضرت در روز عرفه هفتم زیارت
+                      <span className="white-space"> </span>
+                      <span className="textFinded">
+                        <Link href={"./"}>عاشورا</Link>
+                      </span>
+                      <span className="white-space"> </span>
+                      <span className="three-point">...</span>
+                    </p>
+                  </div>
+                </div>
+                <div className="textFind-title-desciption p-3">
+                  <div className="title-textFind">
+                    زیارت امام حسین(ع) در روز
+                    <span className="white-space"> </span>
+                    <span className="textFinded">
+                      <Link href={"./"}>عاشورا</Link>
+                    </span>
+                    <span className="white-space"> </span>
+                  </div>
+                  <div className="description-textFind">
+                    <p>
+                      <span className="three-point">...</span>
+                      <span className="white-space"> </span>
+                      بدان که زیارت روایتشده برای روز{" "}
+                      <span className="textFinded">
+                        <Link href={"./"}>عاشورا</Link>
+                      </span>{" "}
+                      چند زیارت است و ما در اینجا به خاطر رعایت مختصرنویسی فقط
+                      به بیان<span className="three-point">...</span>ما در اینجا
+                      به خاطر رعایت مختصرنویسی فقط به بیان اعمال روز عاشورا نیز
+                      زیارتی را نقل کردم
+                    </p>
+                  </div> 
+                </div>
+                <div className="textFind-title p-3">
+                  <div className="titleOf-textFind">
+                    زیارت
+                    <span className="white-space"> </span>
+                    <span className="textFinded">
+                      <Link href={"./"}>عاشورا</Link>
+                    </span>
+                    <span className="white-space"> </span>
+                    امام حسین
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-          <div class="accordion-item accordion-item-sr">
-            <h2 class="accordion-header accordion-header-sr">
+          <div className="accordion-item accordion-item-sr">
+            <h2 className="accordion-header accordion-header-sr">
               <button
-                class="accordion-button accordion-button-sr d-flex justify-content-between align-items-center"
+                className="accordion-button accordion-button-sr d-flex justify-content-between align-items-center"
                 type="button"
                 data-bs-toggle="collapse"
                 data-bs-target="#panelsStayOpen-collapseTwo"
@@ -167,27 +248,93 @@ export default function SearchResaultPage() {
                 aria-controls="panelsStayOpen-collapseTwo"
               >
                 الأعمال
-              <i className="fa fa-chevron-left icon-accordion-sr"></i>
+                <i className="fa fa-chevron-left icon-accordion-sr"></i>
               </button>
             </h2>
             <div
               id="panelsStayOpen-collapseTwo"
-              class="accordion-collapse collapse show"
+              className="accordion-collapse collapse show"
             >
-              <div class="accordion-body accordion-body-s">
-                <strong>This is the second item's accordion body.</strong> It is
-                hidden by default, until the collapse plugin adds the
-                appropriate classes that we use to style each element. These
-                classes control the overall appearance, as well as the showing
-                and hiding via CSS transitions. You can modify any of this with
-                custom CSS or overriding our default variables. It's also worth
-                noting that just about any HTML can go within the{" "}
-                <code>.accordion-body</code>, though the transition does limit
-                overflow.
+              <div className="accordion-body accordion-body-sr">
+              <div className="textFind-description p-3">
+                  <div className="title-textFind">
+                    زیارت‌های امام حسین(ع) در ایام خاص
+                  </div>
+                  <div className="description-textFind">
+                    <p>
+                      <span className="three-point">...</span>
+                      <span className="white-space"> </span>
+                      شب های قدر پنجم زیارت آن حضرت در عید فطر و قربان ششم زیارت
+                      آن حضرت در روز عرفه هفتم زیارت
+                      <span className="white-space"> </span>
+                      <span className="textFinded">
+                        <Link href={"./"}>عاشورا</Link>
+                      </span>
+                      <span className="white-space"> </span>
+                      <span className="three-point">...</span>
+                    </p>
+                  </div>
+                </div>
+                <div className="textFind-description p-3">
+                  <div className="title-textFind">
+                    زیارت‌های امام حسین(ع) در ایام خاص
+                  </div>
+                  <div className="description-textFind">
+                    <p>
+                      <span className="three-point">...</span>
+                      <span className="white-space"> </span>
+                      اعمال روز عاشورا
+... ماتم داشته باشند به صورتی که در ماتم عزیزترین اولاد و اقوام خود اشتغال میورزند و آن حضرت را به زیارت عاشورا ... از حضرت رضا علیه السلام روایت شده هرکه کوشیدن در حوائج خود را در روز عاشورا ترک کند و به دنبال کاری ... السما یوم عاشورا یا مسکن سفینه نوح علی الجودی یوم عاشورا یا غیاث ابراهیم من النار یوم عاشورا ... ادریس به آسمان در روز عاشورا ای آرامدهنده کشتی نوح بر جودی در روز عاشورا ای فریادرس ابراهیم از آتش در ... روز عاشورا ...
+                      <span className="white-space"> </span>
+                      <span className="textFinded">
+                        <Link href={"./"}>عاشورا</Link>
+                      </span>
+                      <span className="white-space"> </span>
+                      <span className="three-point">...</span>
+                    </p>
+                  </div>
+                </div>
+              <div className="textFind-description p-3">
+                  <div className="title-textFind">
+                    زیارت‌های امام حسین(ع) در ایام خاص
+                  </div>
+                  <div className="description-textFind">
+                    <p>
+                      <span className="three-point">...</span>
+                      <span className="white-space"> </span>
+                      شب های قدر پنجم زیارت آن حضرت در عید فطر و قربان ششم زیارت
+                      آن حضرت در روز عرفه هفتم زیارت
+                      <span className="white-space"> </span>
+                      <span className="textFinded">
+                        <Link href={"./"}>عاشورا</Link>
+                      </span>
+                      <span className="white-space"> </span>
+                      <span className="three-point">...</span>
+                    </p>
+                  </div>
+                </div>
+                <div className="textFind-description p-3">
+                  <div className="title-textFind">
+                    زیارت‌های امام حسین(ع) در ایام خاص
+                  </div>
+                  <div className="description-textFind">
+                    <p>
+                      <span className="three-point">...</span>
+                      <span className="white-space"> </span>
+                      اعمال روز عاشورا
+... ماتم داشته باشند به صورتی که در ماتم عزیزترین اولاد و اقوام خود اشتغال میورزند و آن حضرت را به زیارت عاشورا ... از حضرت رضا علیه السلام روایت شده هرکه کوشیدن در حوائج خود را در روز عاشورا ترک کند و به دنبال کاری ... السما یوم عاشورا یا مسکن سفینه نوح علی الجودی یوم عاشورا یا غیاث ابراهیم من النار یوم عاشورا ... ادریس به آسمان در روز عاشورا ای آرامدهنده کشتی نوح بر جودی در روز عاشورا ای فریادرس ابراهیم از آتش در ... روز عاشورا ...
+                      <span className="white-space"> </span>
+                      <span className="textFinded">
+                        <Link href={"./"}>عاشورا</Link>
+                      </span>
+                      <span className="white-space"> </span>
+                      <span className="three-point">...</span>
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-          
         </div>
         {/* Second Section */}
       </main>
