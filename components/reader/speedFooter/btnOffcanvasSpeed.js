2a902651bc0c1be230f25c4d253d9a1bd7e49b00
mostafa <mostafa.h.web@gmail.com>
2023-12-17 19:08:55 +0300

    fix the bug in speed btn component and finish it

--- components/reader/speedFooter/btnOffcanvasSpeed.js
+++ components/reader/speedFooter/btnOffcanvasSpeed.js
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function BtnOffcanvasSpeed() {
+
+    useEffect(() => {
+        
+        const speedBtn = document.querySelector(".speed-btn");
+        const offcanvasBottomSpeed = document.querySelector(".offcanvas-speed");
+        const header = document.querySelector(".header");
+
+        offcanvasBottomSpeed.addEventListener("show.bs.offcanvas", function () {
+            speedBtn.classList.add("footer-buttons-select");
+            header.classList.add("z-1000");
+        })
+        
+        offcanvasBottomSpeed.addEventListener("hide.bs.offcanvas", function () {
+            speedBtn.classList.remove("footer-buttons-select");
+            header.classList.remove("z-1000");
+        })        
+
+    },[])
+
   return (
     <button
       className="btn speed-btn"
